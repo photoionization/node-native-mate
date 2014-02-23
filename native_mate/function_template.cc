9ec87a04557953531c25ce32939f34c3cafaccd5
Cheng Zhao <zcbenz@gmail.com>
2014-02-23 13:52:13 +0800

    Add Chromium's license.

--- native_mate/function_template.cc
+++ native_mate/function_template.cc
@@ -1,6 +1,6 @@
 // Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE.chromium file.
 
 #include "native_mate/function_template.h"
 
