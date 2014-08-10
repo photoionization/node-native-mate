fcac21aafdb7da8e7e6cfd7528906163ebb55af3
Cheng Zhao <zcbenz@gmail.com>
2014-08-10 19:16:39 +0800

    Add callback converter support.

--- tests/compilation.cc
+++ tests/compilation.cc
@@ -1,4 +1,5 @@
 #include "native_mate/arguments.h"
+#include "native_mate/callback.h"
 #include "native_mate/constructor.h"
 #include "native_mate/converter.h"
 #include "native_mate/dictionary.h"
@@ -18,4 +19,8 @@ void TestCompilation() {
   mate::CreateFunctionTemplate(isolate, base::Bind(TestCompilation));
   mate::ConvertFromV8(isolate, MATE_OBJECT_NEW(isolate), &dictionary);
   mate::ScopedPersistent<v8::Object> object(MATE_OBJECT_NEW(isolate), isolate);
+
+  base::Closure callback;
+  mate::ConvertFromV8(isolate, MATE_OBJECT_NEW(isolate), &callback);
+  mate::ConvertToV8(isolate, base::Bind(&TestCompilation));
 }
